24954fe711852d58f32821ae098709ce34fe03e0
Torsten Dittmann <torsten.dittmann@googlemail.com>
2022-03-02 12:27:18 +0100

    feat: support for 0.13.0

--- src/Appwrite/Client.php
+++ src/Appwrite/Client.php
@@ -37,7 +37,7 @@ class Client
      */
     protected $headers = [
         'content-type' => '',
-        'x-sdk-version' => 'appwrite:php:3.0.0',
+        'x-sdk-version' => 'appwrite:php:4.0.0',
     ];
 
     /**
