cf4c49608846bfacf18e291a6d1c668e896772c5
root <chiragaggarwal5k@gmail.com>
2026-03-25 07:19:29 +0000

    chore: update PHP SDK to 21.0.0

--- src/Appwrite/Enums/VCSReferenceType.php
+++ src/Appwrite/Enums/VCSReferenceType.php
@@ -8,6 +8,7 @@ class VCSReferenceType implements JsonSerializable
 {
     private static VCSReferenceType $BRANCH;
     private static VCSReferenceType $COMMIT;
+    private static VCSReferenceType $TAG;
 
     private string $value;
 
@@ -39,5 +40,12 @@ class VCSReferenceType implements JsonSerializable
             self::$COMMIT = new VCSReferenceType('commit');
         }
         return self::$COMMIT;
+    }
+    public static function TAG(): VCSReferenceType
+    {
+        if (!isset(self::$TAG)) {
+            self::$TAG = new VCSReferenceType('tag');
+        }
+        return self::$TAG;
     }
 }
